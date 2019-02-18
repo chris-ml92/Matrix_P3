357439faad3b53b4d9424f34966e0add6c5cfc87
Francesco Busolin <francesco.busolin@libero.it>
2019-02-18 10:20:16 +0100

    test menu

--- example4.cc
+++ example4.cc
@@ -148,95 +148,80 @@ for (int i=0; i!=4; ++i) {
 std::cout << std::endl;
 */
 // ----------------------------------------------->
-std::cout << "======= NEW TEST SECTION =======\n";
-
-unsigned const m = 4;
-unsigned const n = 4;
+unsigned m, n;
 bool verbose = false;
-
-double etime = omp_get_wtime();
-
-matrix<int> MM(m,n);
-for (int i=0; i!=m; ++i)
-	for(int j=0; j!=n; ++j)
-	  MM(i,j) = (i+1)*(j+1);
-
-matrix<int> MMMM(n,m);
-for (int i=0; i!=n; ++i)
-	for(int j=0; j!=m; ++j)
-	  MMMM(i,j) = (i+1)*(j+1);
-
-matrix<int> B(n, m);
-for (int i = 0; i != n; ++i)
-for (int j = 0; j != m; ++j)
-B(i, j) = (j + 1);
-
-matrix<int> A(n, m);
-for (int i = 0; i != n; ++i)
-for (int j = 0; j != m; ++j)
-A(i, j) = (i + 1);
-
-matrix<int> C(n, m);
-for (int i = 0; i != n; ++i)
-for (int j = 0; j != m; ++j)
-C(i, j) = (i + 1)*(j + 1);
-
-matrix<int,m,n> ZZ = MM*MMMM*MM;
-
-/*if (verbose) {
-	for (int i=0; i!=m; ++i) {
-		for(int j=0; j!=n; ++j)
-			std::cout << ZZ(i,j) << ' ';
-		std::cout << '\n';
-	}
-}*/
-
+std::cout << "======= NEW TEST SECTION =======\n";
 std::cout << std::endl;
+const char* menu = R"(Select Test case: 
+	[Parallellism]
+		1- small operations  [A op B]: A = (32x32), B = (32x32);
+		2- medium operations [A op B]: A = (256x256), B = (256x256) 
+		3- large operations  [A op B]: A = (1024x1024), B = (1024x1024)
+	[Concurrency]
+		4- small sized chain [A op B op C]
+		5- medium sized chain [A op B op C op D op E]
+		6- large sized chain [A op B op C op D op E op F op G op H]
+)";
+std::cout << menu << std::endl;
+
+unsigned choiche;
+std::cin >> choiche;
+std::cout << "Option chosen: " << choiche<< std::endl;
+double etime = omp_get_wtime();
+// empty case
+m = n = 1;
+if (choiche == 4 || choiche == 5 || choiche == 6) {
+	unsigned s;
+	std::cout << "Choose size of matrices: ";
+	std::cin >> s;
+	std::cout << std::endl;
+	m = n = s;
+}
+else if (choiche == 1) { m = n = 32; }
+else if (choiche == 2) { m = n = 128; }
+else if (choiche == 3) { m = n = 1024; }
+matrix<int> A(m, n);
+matrix<int> B(m, n);
+for (int i = 0; i < m; i++) {
+	for (int j = 0; j < n; j++) {
+		A(i, j) = 1;
+		B(i, j) = 1;
+	}
+}
+matrix<int> S, M;
+switch (choiche)
+{
+case 1:
+case 2:
+case 3:
+	etime = omp_get_wtime();
+	S = A + B;
+	M = A * B;
+	break;
+case 4:
+	etime = omp_get_wtime();
+	S = A + B + A;
+	M = A * B * A;
+	break;
+case 5:
+
+	etime = omp_get_wtime();
+	S = A + B + A + B + A;
+	M = A * B * A * B * A;
+	break;
+case 6:
+	etime = omp_get_wtime();
+	S = A + B + A + B + A + B + A + B;
+	M = A * B * A * B * A * B * A * B;
+	break;
+default:
+	break;
+}
 etime = omp_get_wtime() - etime;
 std::cout << "Time elapsed: " << etime << " seconds." << std::endl;
 
-/*for (int i = 0; i != m; ++i) {
-	for (int j = 0; j != n; ++j)
-		std::cout << A(i, j) << ' ';
-	std::cout << '\n';
-}std::cout << '\n';
-for (int i = 0; i != m; ++i) {
-	for (int j = 0; j != n; ++j)
-		std::cout << B(i, j) << ' ';
-	std::cout << '\n';
-}std::cout << '\n';
 
-for (int i = 0; i != m; ++i) {
-	for (int j = 0; j != n; ++j)
-		std::cout << C(i, j) << ' ';
-	std::cout << '\n';
-}*/
-
-matrix<int> SS = ( A + B ) + C + A  ;
-for (int i = 0; i != m; ++i) {
-	for (int j = 0; j != n; ++j)
-		std::cout << SS(i, j) << ' ';
-	std::cout << '\n';
-}
-matrix<int> AMM = A * B * C;
-for (int i = 0; i != m; ++i) {
-	for (int j = 0; j != n; ++j)
-		std::cout << AMM(i, j) << ' ';
-	std::cout << '\n';
-}
-
-matrix<int> SD = (A + B) * (C + SS);
-for (int i = 0; i != m; ++i) {
-	for (int j = 0; j != n; ++j)
-		std::cout << AMM(i, j) << ' ';
-	std::cout << '\n';
-}
-// Testing Async
-/*
-auto x = std::async(sum, MM, MM);
-auto y = std::async(sum, A, B);
-std::this_thread::sleep_for(std::chrono::milliseconds(50));
-matrix<int> C = (x.get())*(y.get());*/
-
+std::getchar();
+std::getchar();
 return 0;
 }
